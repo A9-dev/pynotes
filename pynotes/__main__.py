29b70b677112933e9fc2279f4296f155d4002d62
Mogzhey-dev <mogzhey@gmail.com>
2020-05-05 13:59:05 +0100

    added argparse

--- pynotes/__main__.py
+++ pynotes/__main__.py
@@ -1,14 +1,12 @@
 import sys
 import win10toast
-
+import argparse
 
 def main():
-    print('in main')
-    args = sys.argv[1:]
-    print('count of args :: {}'.format(len(args)))
-    for arg in args:
-        print('passed argument :: {}'.format(arg))
+    parser = argparse.ArgumentParser()
 
+    parser.add_argument("-q", help="Add a new project", action="store_true") # specify that the command should be run quietly, i.e. give no console output.
+    
 
 if __name__ == '__main__':
     main()
