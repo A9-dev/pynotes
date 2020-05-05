295c91985dd0e200a3db5987a3e366c9957c692b
Mogzhey-dev <mogzhey@gmail.com>
2020-05-05 15:31:12 +0100

    added command line arguments

--- pynotes/__main__.py
+++ pynotes/__main__.py
@@ -1,23 +1,26 @@
 import sys
 import win10toast
 import argparse
+import os
 
 def main():
     parser = argparse.ArgumentParser(description="add a description Henry, I'm no good with words")
     commands_parser = parser.add_subparsers(help='commands')
 
-    parser.add_argument("-q", "--quiet", help="Run command with no output",
-    					action="store_true") # specify that the command should be run quietly, i.e. give no console output.
+    parser.add_argument("-q", "--quiet", help="Run command with no output", action="store_true")
 
-    init_parser = commands_parser.add_parser("init", help="")
+    init_parser = commands_parser.add_parser("initdir", help="")
+
+    add_project_parser = commands_parser.add_parser("add-project", help="")
+    add_project_parser.add_argument("projectName", help="")
+
+    add_note_parser = commands_parser.add_parser("add-note", help="")
+    add_note_parser.add_argument("note", help="")
+    add_note_parser.add_argument("-p", "--project", help="", action="store", type=str)
 
     view_parser = commands_parser.add_parser("view", help="")
-    view_parser.add_argument("--all", default=False, action="store_true")
-
-    add_note_parser = commands_parser.add_parser("addnote", help="")
-    add_note_parser.add_argument("title", help="")
-    add_note_parser.add_argument("-m", help="",
-    							action="store", type=str)
+    view_parser.add_argument("-a", "--all", default=False, action="store_true")
+    view_parser.add_argument("-p", "--project", help="", action="store", type=str)
 
     print(parser.parse_args())
 
